e738104ebc607b8f079747f57eb7174d3b662ac8
Mikeqin <Fengling.Qin@gmail.com>
2016-10-10 23:05:46 +0800

    Init commit

---
+++ debug.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+
+import re
+import sys
+import json
+
+from cgminer_api import cgminer_api
+
+pattern = re.compile(r'.*DNA\[(?P<dna>[^\]]+)\].*MW\[(?P<mw>[^]]+)\].*PVT_T\[(?P<pvt_t>[^]]+)\]')
+
+def debug(ip):
+    global pattern
+    js = cgminer_api(ip, 4028, ['estats'])
+
+    for estat in sorted(js['STATS'], key=lambda k: k['STATS']):
+        for mm in sorted(estat):
+            if mm[:5] == 'MM ID' and re.match(pattern, estat[mm]) is not None:
+                g = re.match(pattern, estat[mm]).groupdict()
+                print '[{:>13}][{:>2}][{:>2}]\tDNA[{}]\tMW[{}]\tPVT_T[{}]'.format(
+                        ip, estat['ID'][3:], mm[5:], g['dna'], g['mw'], g['pvt_t'])
+
+if __name__ == '__main__':
+    ip = sys.argv[1]
+    try:
+        port = int(sys.argv[2])
+    except IndexError:
+        port = 4028
+    cgminer_api(ip, port, ['debug', 'D'])
+    debug(ip)
+    cgminer_api(ip, port, ['debug', 'D'])
+
+
+
+    
---
+++ gen_rpt.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python debug.py 192.168.1.24
